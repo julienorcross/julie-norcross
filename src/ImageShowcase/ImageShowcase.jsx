6cf95a0538d5368b611250cdfd018f98e81887ab
Julie Norcross <julesnorc@gmail.com>
2019-08-01 21:25:52 -0400

    selected class functionality and styles

--- src/ImageShowcase/ImageShowcase.jsx
+++ src/ImageShowcase/ImageShowcase.jsx
@@ -7,23 +7,35 @@ class ImageShowcase extends Component {
     this.state = { main: this.props.imgs[0] };
   }
 
+  static defaultProps = { selected: 0 };
+
+  setThumbnails() {
+    let arr = [];
+    for (let i = 0; i < this.props.imgs.length; i++) {
+      arr.push(
+        <div>
+          <img
+            src={this.props.imgs[i]}
+            className={this.props.imgs[i] === this.state.main ? 'selected' : ''}
+            onClick={e => this.switchMain(this.props.imgs[i])}
+          />
+        </div>
+      );
+    }
+    return arr;
+  }
+
   switchMain(path) {
     this.setState({ main: path });
   }
 
   render() {
-    const thumbnails = this.props.imgs.map(path => (
-      <img
-        src={path}
-        className="opera-spread"
-        onClick={e => this.switchMain(path)}
-      />
-    ));
+    const thumbnails = this.setThumbnails();
 
     return (
       <div className="showcase">
-        <img src={this.state.main} className="opera-spread-main" alt="" />
-        {thumbnails}
+        <img src={this.state.main} className="showcase-main" alt="" />
+        <div className="showcase-thumbnails">{thumbnails}</div>
       </div>
     );
   }
