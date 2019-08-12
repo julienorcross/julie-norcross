8079db60e703f453d4fbf4c00c386ae46ae25f60
Julie Norcross <julesnorc@gmail.com>
2019-08-11 21:06:34 -0400

    fixed height on carousel

--- src/Work/WorkItem/WorkItem.jsx
+++ src/Work/WorkItem/WorkItem.jsx
@@ -1,36 +1,66 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './WorkItem.scss';
 import WorkIntro from '../WorkIntro/WorkIntro';
-import ImageShowcase from '../ImageShowcase/ImageShowcase';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import { Carousel } from 'react-responsive-carousel';
 
-const WorkItem = props => {
-  const item = props.items.find(findItem);
+let width = 0;
 
-  function findItem(workItem) {
-    return workItem.slug === props.match.params.workSlug;
+class WorkItem extends Component {
+  constructor(props) {
+    super(props);
+    this.myInput = React.createRef();
+    this.state = { width: window.innerWidth };
+    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
   }
 
-  // function calculateImageHeight(workItem) {
-  //   workItem.aspectRatio;
-  // }
+  componentDidMount() {
+    this.updateWindowDimensions();
+    window.addEventListener('resize', this.updateWindowDimensions);
+  }
 
-  return (
-    <div className="work-item-wrapper">
-      <WorkIntro item={item} />
-      <Carousel>
-        {item.images.map(imgPath => {
-          return (
-            <div style={{ height: 400 }}>
-              <img src={imgPath} />
-            </div>
-          );
-        })}
-      </Carousel>
-      {/* <ImageShowcase imgs={item.images} /> */}
-    </div>
-  );
-};
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.updateWindowDimensions);
+  }
+
+  updateWindowDimensions() {
+    this.setState({ width: window.innerWidth });
+  }
+
+  findItem = workItem => {
+    return workItem.slug === this.props.match.params.workSlug;
+  };
+
+  calculateImageHeight = item => {
+    if (this.state.width >= 964) {
+      return item.aspectRatio * 900;
+    } else {
+      return item.aspectRatio * (this.state.width - 64);
+    }
+  };
+
+  render() {
+    const item = this.props.items.find(this.findItem);
+    const height = this.calculateImageHeight(item);
+    return (
+      <div className="work-item-wrapper">
+        <WorkIntro item={item} />
+        <Carousel showArrows={false} showIndicators={false}>
+          {item.images.map(imgPath => {
+            return (
+              <div
+                className="custom-slide"
+                ref={this.myInput}
+                style={{ height }}>
+                <img src={imgPath} />
+              </div>
+            );
+          })}
+        </Carousel>
+        {/* <ImageShowcase imgs={item.images} /> */}
+      </div>
+    );
+  }
+}
 
 export default WorkItem;
