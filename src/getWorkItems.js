675d642ba13d02c0d01fabd28922de7941c4d812
Julie Norcross <julesnorc@gmail.com>
2019-11-05 20:28:28 -0500

    tweak to syntax in fetch function

--- src/getWorkItems.js
+++ src/getWorkItems.js
@@ -18,13 +18,11 @@ async function getWorkItems() {
           alt: featured_image.alt,
           sizes: featured_image.sizes
         },
-        slideshow: acf.slideshow.map(slide => {
-          return {
-            url: slide.url,
-            alt: slide.alt,
-            sizes: slide.sizes
-          };
-        })
+        slideshow: slideshow.map(slide => ({
+          url: slide.url,
+          alt: slide.alt,
+          sizes: slide.sizes
+        }))
       };
     })
     .sort((a, b) => a.sortOrder - b.sortOrder);
