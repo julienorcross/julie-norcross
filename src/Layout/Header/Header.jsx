39427e9e116c74aacf36a8cf558d803f801595c0
Julie Norcross <julesnorc@gmail.com>
2019-11-13 20:07:33 -0500

    fixed transitions and hover states on mobile menu

--- src/Layout/Header/Header.jsx
+++ src/Layout/Header/Header.jsx
@@ -1,48 +1,10 @@
-import React, { Component } from 'react';
-import './Header.scss';
-import Menu from '../Menu/Menu';
-import logo from '../../img/Logo_primary_thicc.svg';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import "./Header.scss";
+import Menu from "../Menu/Menu";
+import logo from "../../img/Logo_primary_thicc.svg";
+import { Link } from "react-router-dom";
 
 class Header extends Component {
-  constructor(props) {
-    super(props);
-    this.myInput = React.createRef();
-
-    this.state = { isOpen: false, width: window.innerWidth };
-    this.toggleMenu = this.toggleMenu.bind(this);
-    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
-  }
-
-  componentDidMount() {
-    this.updateWindowDimensions();
-    window.addEventListener('resize', this.updateWindowDimensions);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.updateWindowDimensions);
-  }
-
-  updateWindowDimensions() {
-    this.setState({ width: window.innerWidth });
-  }
-
-  toggleMenu() {
-    if (this.state.width >= 768) {
-      this.setState({ isOpen: false });
-    } else {
-      this.setState({ isOpen: !this.state.isOpen });
-    }
-
-    //   , () => {
-    //   if (this.state.isOpen) {
-    //     document.body.style.overflow = 'hidden';
-    //   } else {
-    //     document.body.style.overflow = 'scroll';
-    //   }
-    // });
-  }
-
   render() {
     return (
       <header className="header-container">
@@ -54,11 +16,7 @@ class Header extends Component {
             <Link to="/">Julie Norcross</Link>
           </h1>
         </div>
-        <Menu
-          className="Menu"
-          isOpen={this.state.isOpen}
-          handleToggle={this.toggleMenu}
-        />
+        <Menu className="Menu" />
       </header>
     );
   }
--- src/Layout/Menu/Menu.jsx
+++ src/Layout/Menu/Menu.jsx
@@ -1,26 +1,18 @@
-import React, { Component } from 'react';
-import { Link, withRouter } from 'react-router-dom';
-import './Menu.scss';
+import React, { Component } from "react";
+import { Link, withRouter } from "react-router-dom";
+import "./Menu.scss";
 
 class Menu extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   this.state = { isOpen: false };
-  // }
-
   static defaultProps = {
     pages: [
-      { title: 'Home', path: '/' },
-      { title: 'About', path: '/about' },
-      { title: 'Contact', path: '/contact' },
-      { title: 'Github', path: 'https://github.com/julienorcross' }
+      { title: "Home", path: "/" },
+      { title: "About", path: "/about" },
+      { title: "Contact", path: "/contact" },
+      { title: "Github", path: "https://github.com/julienorcross" }
     ]
   };
 
   render() {
-    const menuOpen = `menu-overlay ${
-      this.props.isOpen ? ' menuOpen' : 'menuClosed'
-    }`;
     return (
       <div className="menu-wrapper">
         <input type="checkbox" className="toggler" />
@@ -34,9 +26,9 @@ class Menu extends Component {
               <ul>
                 {this.props.pages.map(page => {
                   const curr = `nav-link ${
-                    this.props.location.pathname === page.path ? 'curr' : ''
+                    this.props.location.pathname === page.path ? "curr" : ""
                   }`;
-                  if (page.title !== 'Github') {
+                  if (page.title !== "Github") {
                     return (
                       <li key={page.path}>
                         <Link to={page.path} className={curr}>
@@ -51,7 +43,8 @@ class Menu extends Component {
                           href={page.path}
                           target="_blank"
                           className={curr}
-                          rel="noopener noreferrer">
+                          rel="noopener noreferrer"
+                        >
                           <i className="fas fa-external-link-alt"></i> Github
                         </a>
                       </li>
