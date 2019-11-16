3e24f38cf00d82a3330d3ef316c868af81394216
Julie Norcross <julesnorc@gmail.com>
2019-11-16 15:46:39 -0500

    submit success page, fixed mobile menu alignment

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -1,34 +1,68 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Contact.scss';
 
-const Contact = () => (
-  <section className="contact">
-    <h2>Contact</h2>
-    <p>I'm all ears.</p>
-    <form name="contact" method="POST" data-netlify="true">
-      <div className="form-group">
-        {/* <label htmlFor="name">Name</label> */}
-        <input type="text" id="name" name="name" placeholder="Name" />
-        <div className="underline" />
+const Contact = () => {
+  const [success, setSuccess] = useState(null);
+  const [error, setError] = useState(null);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    // fetch('https://julienorcross.com/contact.php', { method: 'POST' })
+    //   .then(res => res.text())
+    //   .then(text => console.log(text));
+    setSuccess(true);
+    // setError(true);
+  }
+
+  if (success) {
+    return (
+      <div className="Success">
+        <h2>Got it!</h2>
+        <p>Thanks for your message! I'll get back to you as soon as I can. </p>
       </div>
-      <div className="form-group">
-        {/* <label htmlFor="email">Email</label> */}
-        <input type="email" id="email" name="email" placeholder="Email" />
-        <div className="underline" />
+    );
+  } else if (error) {
+    return (
+      <div className="Error">
+        <h2>Something went wrong</h2>
+        <p>Please go back and try again.</p>
       </div>
-      <div className="form-group">
-        {/* <label htmlFor="subject">Subject</label> */}
-        <input type="text" id="subject" name="subject" placeholder="Subject" />
-        <div className="underline" />
-      </div>
-      <div className="form-group">
-        {/* <label htmlFor="message">Message</label> */}
-        <textarea name="message" cols="30" rows="1" placeholder="Message" />
-        <div className="underline" />
-      </div>
-      <input type="submit" value="Submit" className="button" />
-    </form>
-  </section>
-);
+    );
+  }
+  return (
+    <section className="contact">
+      <h2>Contact</h2>
+      <p>I'm all ears.</p>
+      <form name="contact" onSubmit={handleSubmit}>
+        <div className="form-group">
+          {/* <label htmlFor="name">Name</label> */}
+          <input type="text" id="name" name="name" placeholder="Name" />
+          <div className="underline" />
+        </div>
+        <div className="form-group">
+          {/* <label htmlFor="email">Email</label> */}
+          <input type="email" id="email" name="email" placeholder="Email" />
+          <div className="underline" />
+        </div>
+        <div className="form-group">
+          {/* <label htmlFor="subject">Subject</label> */}
+          <input
+            type="text"
+            id="subject"
+            name="subject"
+            placeholder="Subject"
+          />
+          <div className="underline" />
+        </div>
+        <div className="form-group">
+          {/* <label htmlFor="message">Message</label> */}
+          <textarea name="message" cols="30" rows="1" placeholder="Message" />
+          <div className="underline" />
+        </div>
+        <input type="submit" value="Submit" className="button" />
+      </form>
+    </section>
+  );
+};
 
 export default Contact;
