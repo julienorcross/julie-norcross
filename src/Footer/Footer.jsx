5a4497c06fec654e6f987a90891f07d44d8b1793
Julie Norcross <julesnorc@gmail.com>
2019-11-17 20:13:50 -0500

    converted a few to functional components, renamed images

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './Footer.scss';
 
-const Footer = () => {
-  return <footer className="Footer">&copy; Julie Norcoss, 2019</footer>;
-};
+const Footer = () => (
+  <footer className="Footer">&copy; Julie Norcoss, 2019</footer>
+);
 
 export default Footer;
--- src/Layout/Header/Header.jsx
+++ src/Layout/Header/Header.jsx
@@ -1,25 +1,22 @@
-import React, { Component } from "react";
-import "./Header.scss";
-import Menu from "../Menu/Menu";
-import logo from "../../img/Logo_primary_thicc.svg";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
-class Header extends Component {
-  render() {
-    return (
-      <header className="header-container">
-        <div className="header-brand">
-          <Link to="/">
-            <img className="logo" src={logo} alt="" />
-          </Link>
-          <h1>
-            <Link to="/">Julie Norcross</Link>
-          </h1>
-        </div>
-        <Menu className="Menu" />
-      </header>
-    );
-  }
-}
+import Menu from '../Menu/Menu';
+import logo from '../../img/Logo.svg';
+import './Header.scss';
+
+const Header = () => (
+  <header className="header-container">
+    <div className="header-brand">
+      <Link to="/">
+        <img className="logo" src={logo} alt="" />
+      </Link>
+      <h1>
+        <Link to="/">Julie Norcross</Link>
+      </h1>
+    </div>
+    <Menu className="Menu" />
+  </header>
+);
 
 export default Header;
