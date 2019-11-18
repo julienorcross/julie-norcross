c35a6ca39419e8e0bb4ede30afb928963dade763
Julie Norcross <julesnorc@gmail.com>
2019-11-17 20:02:29 -0500

    converted workintro to functional component

--- src/Work/WorkIntro/WorkIntro.jsx
+++ src/Work/WorkIntro/WorkIntro.jsx
@@ -1,29 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './WorkIntro.scss';
 
-class WorkIntro extends Component {
-  createMarkup() {
-    return { __html: this.props.item.description };
-  }
+const WorkIntro = props => {
+  const createMarkup = () => {
+    return { __html: props.item.description };
+  };
 
-  render() {
-    return (
-      <div className="work-intro-wrapper">
-        <div
-          className="work-intro-img"
-          style={{
-            backgroundImage: `url(${this.props.item.featuredImage.url})`
-          }}
-        />
-        <div className="work-intro-text">
-          <h2 className="work-intro-title">{this.props.item.title}</h2>
-          <h3 className="work-intro-subtitle">// {this.props.item.subtitle}</h3>
-          <div className="description">
-            <div dangerouslySetInnerHTML={this.createMarkup()} />
-          </div>
+  const { featuredImage, title, subtitle } = props.item;
+
+  return (
+    <div className="work-intro-wrapper">
+      <div
+        className="work-intro-img"
+        style={{
+          backgroundImage: `url(${featuredImage.url})`
+        }}
+      />
+      <div className="work-intro-text">
+        <h2 className="work-intro-title">{title}</h2>
+        <h3 className="work-intro-subtitle">// {subtitle}</h3>
+        <div className="description">
+          <div dangerouslySetInnerHTML={createMarkup()} />
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
+
 export default WorkIntro;
