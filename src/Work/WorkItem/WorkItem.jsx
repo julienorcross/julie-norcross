27975b49ba365353caeddf4676b202d566dd9628
Julie Norcross <julesnorc@gmail.com>
2019-11-26 15:43:58 -0500

    convert workitem to hooks

--- src/Work/WorkItem/WorkItem.jsx
+++ src/Work/WorkItem/WorkItem.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Redirect } from 'react-router-dom';
 
 import './WorkItem.scss';
@@ -6,68 +6,61 @@ import WorkIntro from '../WorkIntro/WorkIntro';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
 import { Carousel } from 'react-responsive-carousel';
 
-class WorkItem extends Component {
-  constructor(props) {
-    super(props);
-    this.myInput = React.createRef();
-    this.state = { width: window.innerWidth };
-    this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
-  }
+const WorkItem = ({ item, match }) => {
+  useEffect(() => {
+    // mount
+    updateWindowDimensions();
+    window.addEventListener('resize', updateWindowDimensions);
 
-  componentDidMount() {
-    this.updateWindowDimensions();
-    window.addEventListener('resize', this.updateWindowDimensions);
-  }
+    // unmount
+    return () => window.removeEventListener('resize', updateWindowDimensions);
+  }, []);
 
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.updateWindowDimensions);
-  }
+  // useState
+  const [width, setWidth] = useState(window.innerWidth);
 
-  updateWindowDimensions() {
-    this.setState({ width: window.innerWidth });
-  }
-
-  findItem = workItem => {
-    return workItem.slug === this.props.match.params.workSlug;
+  const updateWindowDimensions = () => {
+    setWidth(window.innerWidth);
   };
 
-  calculateImageHeight = item => {
-    if (this.state.width >= 964) {
+  const findItem = workItem => {
+    return workItem.slug === match.params.workSlug;
+  };
+
+  const calculateImageHeight = item => {
+    if (width >= 964) {
       return item.aspectRatio * 900;
     } else {
-      return item.aspectRatio * (this.state.width - 64);
+      return item.aspectRatio * (width - 64);
     }
   };
 
-  render() {
-    console.log(this.props.item);
-    // const item = this.props.items.find(this.findItem);
-    // console.log(item);
-    const { item } = this.props;
-    if (!item) {
-      return <Redirect to="/404" />;
-    }
-    const height = '500px';
-    // const height = this.calculateImageHeight(item);
-    return (
-      <div className="work-item-wrapper">
-        <WorkIntro item={item} />
-        <Carousel showStatus={false} showArrows={false} showIndicators={false}>
-          {item.slideshow.map(img => {
-            return (
-              <div
-                key={img.url}
-                className="custom-slide"
-                ref={this.myInput}
-                style={{ height }}>
-                <img src={img.url} alt={img.alt} />
-              </div>
-            );
-          })}
-        </Carousel>
-      </div>
-    );
+  // const item = this.props.items.find(this.findItem);
+  // console.log(item);
+  // const { item } = this.props;
+  if (!item) {
+    return <Redirect to="/404" />;
   }
-}
+  const height = '500px';
+  // const height = this.calculateImageHeight(item);
+  return (
+    <div className="work-item-wrapper">
+      <WorkIntro item={item} />
+      <Carousel showStatus={false} showArrows={false} showIndicators={false}>
+        {item.slideshow.map(img => {
+          return (
+            <div
+              key={img.url}
+              className="custom-slide"
+              // ref={myInput}
+              style={{ height }}>
+              <img src={img.url} alt={img.alt} />
+            </div>
+          );
+        })}
+      </Carousel>
+    </div>
+  );
+};
 
 export default WorkItem;
