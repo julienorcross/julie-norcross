1cdc12a61931a82ca64f2118bc396bd2767220b8
Julie Norcross <julesnorc@gmail.com>
2020-01-12 18:36:44 -0500

    updated footer year

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Footer.scss';
 
 const Footer = () => (
-  <footer className="Footer">&copy; Julie Norcoss, 2019</footer>
+  <footer className="Footer">&copy; Julie Norcoss, 2020</footer>
 );
 
 export default Footer;
