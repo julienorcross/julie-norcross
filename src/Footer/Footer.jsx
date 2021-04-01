abfb7c99c9165cf4e03133b14c606a708aaba75d
Julie Norcross <julesnorc@gmail.com>
2021-04-01 18:44:53 -0400

    fixed typo & date in footer

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Footer.scss';
 
 const Footer = () => (
-  <footer className="Footer">&copy; Julie Norcoss, 2020</footer>
+  <footer className="Footer">&copy; Julie Norcross, 2021</footer>
 );
 
 export default Footer;
